90fe9beef778e6a1dd7bc439365fe9ecfa6a91f4
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2026-01-13 10:59:58 -0800

    foundation:syntax

--- learning/hello.go
+++ learning/hello.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 )
+
+func add( x int, y int)  int {
+	return x + y
+}
+
+func name( firstName , lastName string)  string {
+	return  "I am " + firstName + " and my surname is " + lastName
+}
+
+func swap( x , y string) (string, string) {
+	return y, x
+}
+
 
 func main ()  {
 	fmt.Println("Hello, Go!")
 	x:=16.0
 	fmt.Println("The square root of", x, "is", math.Sqrt(x))
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("1 + 1 =", add(1, 1))
+	fmt.Println(name("John", "Doe"))
+	a, b := swap("first", "second")
+	fmt.Println("Swapped:", a, b)
 }
-
